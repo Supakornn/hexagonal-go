d18c4c2afd3da0340b69fc6a3d98d5ede2cba935
supakorn <supakorn0728@gmail.com>
2023-07-27 16:28:38 +0700

    init
    add
    
    update
    
    init
    
    up
    
    commit
    
    readme
    
    read
    
    rm
    
    logger
    
    cors
    
    router check
    
    logger
    
    fix
    
    signup
    
    signin
    
    signin refresh token
    
    signout
    
    signout
    
    userprofile
    
    params check
    
    c
    
    admin
    
    appinfo
    
    apikey
    
    find category
    
    categories
    
    upload file
    
    delete img
    
    product module init
    
    find one product
    
    bug img
    
    find patterns init
    
    find many product
    
    find all products
    
    insert product
    
    delete product
    
    orders
    
    docker
    
    test
    
    Update README.md
    
    Bump github.com/gofiber/fiber/v2 from 2.48.0 to 2.49.2
    
    Bumps [github.com/gofiber/fiber/v2](https://github.com/gofiber/fiber) from 2.48.0 to 2.49.2.
    - [Release notes](https://github.com/gofiber/fiber/releases)
    - [Commits](https://github.com/gofiber/fiber/compare/v2.48.0...v2.49.2)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/gofiber/fiber/v2
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump golang.org/x/net from 0.12.0 to 0.17.0
    
    Bumps [golang.org/x/net](https://github.com/golang/net) from 0.12.0 to 0.17.0.
    - [Commits](https://github.com/golang/net/compare/v0.12.0...v0.17.0)
    
    ---
    updated-dependencies:
    - dependency-name: golang.org/x/net
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Update README.md
    
    Bump google.golang.org/grpc from 1.55.0 to 1.56.3
    
    Bumps [google.golang.org/grpc](https://github.com/grpc/grpc-go) from 1.55.0 to 1.56.3.
    - [Release notes](https://github.com/grpc/grpc-go/releases)
    - [Commits](https://github.com/grpc/grpc-go/compare/v1.55.0...v1.56.3)
    
    ---
    updated-dependencies:
    - dependency-name: google.golang.org/grpc
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump golang.org/x/crypto from 0.14.0 to 0.17.0
    
    Bumps [golang.org/x/crypto](https://github.com/golang/crypto) from 0.14.0 to 0.17.0.
    - [Commits](https://github.com/golang/crypto/compare/v0.14.0...v0.17.0)
    
    ---
    updated-dependencies:
    - dependency-name: golang.org/x/crypto
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump github.com/gofiber/fiber/v2 from 2.49.2 to 2.52.1
    
    Bumps [github.com/gofiber/fiber/v2](https://github.com/gofiber/fiber) from 2.49.2 to 2.52.1.
    - [Release notes](https://github.com/gofiber/fiber/releases)
    - [Commits](https://github.com/gofiber/fiber/compare/v2.49.2...v2.52.1)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/gofiber/fiber/v2
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump github.com/jackc/pgx/v5 from 5.4.2 to 5.5.4
    
    Bumps [github.com/jackc/pgx/v5](https://github.com/jackc/pgx) from 5.4.2 to 5.5.4.
    - [Changelog](https://github.com/jackc/pgx/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/jackc/pgx/compare/v5.4.2...v5.5.4)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/jackc/pgx/v5
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump golang.org/x/net from 0.17.0 to 0.23.0
    
    Bumps [golang.org/x/net](https://github.com/golang/net) from 0.17.0 to 0.23.0.
    - [Commits](https://github.com/golang/net/compare/v0.17.0...v0.23.0)
    
    ---
    updated-dependencies:
    - dependency-name: golang.org/x/net
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump github.com/gofiber/fiber/v2 from 2.52.1 to 2.52.5
    
    Bumps [github.com/gofiber/fiber/v2](https://github.com/gofiber/fiber) from 2.52.1 to 2.52.5.
    - [Release notes](https://github.com/gofiber/fiber/releases)
    - [Commits](https://github.com/gofiber/fiber/compare/v2.52.1...v2.52.5)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/gofiber/fiber/v2
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    up
    
    up

---
+++ README.md
@@ -0,0 +1,46 @@
+# Start DB
+```
+docker run --name db -e POSTGRES_USER=? -e POSTGRES_PASSWORD=? -p 4444:5432 -d postgres:alpine
+docker exec -it db
+psql -U USER?
+CREATE DATABASE db;
+```
+# Migrate DV
+```
+migrate -database 'postgres://user?:pass?@0.0.0.0:4444/db?sslmode=disable' -source file://? -verbose up
+migrate -database 'postgres://user?:pass?@0.0.0.0:4444/db?sslmode=disable' -source file://? -verbose down
+```
+# Build
+```
+docker build -t asia.gcr.io/project-id/container-bucket .
+docker push asia.gcr.io/project-id/container-bucket
+gcloud auth configure-docker
+```
+
+# ENV
+```ENV
+APP_HOST=
+APP_PORT=
+APP_NAME=
+APP_VERSION=
+APP_BODY_LIMIT=
+APP_READ_TIMEOUT=
+APP_WRTIE_TIMEOUT=
+APP_FILE_LIMIT=
+APP_GCP_BUCKET=
+
+JWT_ADMIN_KEY=
+JWT_SECRET_KEY=
+JWT_API_KEY=
+JWT_ACCESS_EXPIRES=
+JWT_REFRESH_EXPIRES=
+
+DB_HOST=
+DB_PORT=
+DB_PROTOCOL=
+DB_USERNAME=
+DB_PASSWORD=
+DB_DATABASE=
+DB_SSL_MODE=
+DB_MAX_CONNECTIONS=
+```
---
+++ pkg/databaes/migrations/000001_go_api_db.down.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+
+DROP TABLE IF EXISTS set_updated_at_timestamp_users_table ON "users";
+DROP TABLE IF EXISTS  set_updated_at_timestamp_oauth_table ON "oauth";
+DROP TABLE IF EXISTS  set_updated_at_timestamp_images_table ON "images";
+DROP TABLE IF EXISTS  set_updated_at_timestamp_products_table ON "products";
+DROP TABLE IF EXISTS  set_updated_at_timestamp_orders_table ON "orders";
+
+DROP FUNCTION IF EXISTS set_updated_at_column();
+
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "oauth" CASCADE;
+DROP TABLE IF EXISTS "roles" CASCADE;
+DROP TABLE IF EXISTS "products" CASCADE;
+DROP TABLE IF EXISTS "categories" CASCADE;
+DROP TABLE IF EXISTS "prodcuts_orders" CASCADE;
+DROP TABLE IF EXISTS "images" CASCADE;
+DROP TABLE IF EXISTS "orders" CASCADE;
+DROP TABLE IF EXISTS "products_categories" CASCADE;
+
+DROP SEQUENCE IF EXISTS users_id_seq;
+DROP SEQUENCE IF EXISTS products_id_seq;
+DROP SEQUENCE IF EXISTS orders_id_seq;
+
+DROP TYPE IF EXISTS "order_status";
+
+COMMIT;
