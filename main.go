eea953cb678b0cdf54dc9a5862c344c08441b63b
Supakorn <supakorn0728@gmail.com>
2024-09-25 12:00:48 +0700

    update :star:

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"os"
+
+	"github.com/Supakornn/go-api/config"
+	"github.com/Supakornn/go-api/pkg/databaes"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
 func main() {
-	println("Hello, World!")
+	cfg := config.LoadConfig(envPath())
+	db := databaes.DbConnect(cfg.Db())
+	defer db.Close()
 }
--- pkg/databaes/migrations/000001_go_api_db.up.sql
+++ pkg/databaes/migrations/000001_go_api_db.up.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SET TIME ZONE 'Asia/Bangkok';
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE SEQUENCE users_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE products_id_seq START WITH 1 INCREMENT BY 1;
@@ -13,7 +13,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = now();
     RETURN NEW;
-END
+END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE TYPE "order_status" AS ENUM (
@@ -21,10 +21,10 @@ CREATE TYPE "order_status" AS ENUM (
     'shipping',
     'completed',
     'canceled'
-)
+);
 
 CREATE TABLE "users" (
-  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT("U", LPAD(NEXTVAL("users_id_seq")::TEXT, 6, '0')),
+  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT('U', LPAD(NEXTVAL('users_id_seq')::TEXT, 6, '0')),
   "username" VARCHAR UNIQUE NOT NULL,
   "password" VARCHAR NOT NULL,
   "email" VARCHAR UNIQUE NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE "users" (
 );
 
 CREATE TABLE "oauth" (
-  "id" uuid NOT NULL UNIQUE VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "id" uuid NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
   "userid" VARCHAR NOT NULL,
   "access_token" VARCHAR NOT NULL,
   "refresh_token" VARCHAR NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE "roles" (
 );
 
 CREATE TABLE "products" (
-  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT("P", LPAD(NEXTVAL("products_id_seq")::TEXT, 6, '0')),
+  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT('P', LPAD(NEXTVAL('products_id_seq')::TEXT, 6, '0')),
   "title" VARCHAR NOT NULL,
   "description" VARCHAR NOT NULL,
   "price" float NOT NULL DEFAULT 0,
@@ -57,7 +57,7 @@ CREATE TABLE "products" (
 );
 
 CREATE TABLE "images" (
-  "id" uuid NOT NULL UNIQUE VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "id" uuid NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
   "filename" VARCHAR NOT NULL,
   "url" VARCHAR NOT NULL, 
   "product_id" VARCHAR NOT NULL,
@@ -66,9 +66,9 @@ CREATE TABLE "images" (
 );
 
 CREATE TABLE "products_categories" (
-  "id" uuid NOT NULL UNIQUE VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(), 
+  "id" uuid NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(), 
   "product_id" VARCHAR NOT NULL,
-  "category_id" VARCHAR NOT NULL
+  "category_id" INT NOT NULL
 );
 
 CREATE TABLE "categories" (
@@ -77,7 +77,7 @@ CREATE TABLE "categories" (
 );
 
 CREATE TABLE "orders" (
-  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT("O", LPAD(NEXTVAL("orders_id_seq")::TEXT, 6, '0')),
+  "id" VARCHAR(7) PRIMARY KEY DEFAULT CONCAT('O', LPAD(NEXTVAL('orders_id_seq')::TEXT, 6, '0')),
   "user_id" VARCHAR NOT NULL,
   "contact" VARCHAR NOT NULL, 
   "address" VARCHAR NOT NULL, 
@@ -87,8 +87,8 @@ CREATE TABLE "orders" (
   "updated_at" TIMESTAMP NOT NULL DEFAULT now() 
 );
 
-CREATE TABLE "prodcuts_orders" (
-  "id" uuid NOT NULL UNIQUE VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(), 
+CREATE TABLE "products_orders" (
+  "id" uuid NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(), 
   "order_id" VARCHAR NOT NULL,
   "qty" INT NOT NULL DEFAULT 1,
   "product" jsonb
@@ -100,13 +100,13 @@ ALTER TABLE "oauth" ADD FOREIGN KEY ("userid") REFERENCES "users" ("id");
 
 ALTER TABLE "images" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 
-ALTER TABLE "products_categories" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "products_categories" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "products_categories" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+ALTER TABLE "products_categories" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "prodcuts_orders" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+ALTER TABLE "products_orders" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
 
 CREATE TRIGGER set_updated_at_timestamp_users_table BEFORE UPDATE ON "users" FOR EACH ROW EXECUTE PROCEDURE set_updated_at_column();
 CREATE TRIGGER set_updated_at_timestamp_oauth_table BEFORE UPDATE ON "oauth" FOR EACH ROW EXECUTE PROCEDURE set_updated_at_column();
