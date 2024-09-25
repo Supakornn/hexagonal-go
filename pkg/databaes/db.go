571e2ad0ff1bf2a314d7470899174186954bcf02
Supakorn <supakorn0728@gmail.com>
2024-09-25 12:00:48 +0700

    update :star:

--- pkg/databaes/db.go
+++ pkg/databaes/db.go
@@ -1,1 +1,20 @@
 package databaes
+
+import (
+	"log"
+
+	"github.com/Supakornn/go-api/config"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+func DbConnect(cfg config.IDbConfig) *sqlx.DB {
+	db, err := sqlx.Connect("pgx", cfg.Url())
+	if err != nil {
+		log.Fatalf("Cannot connect to database: %v", err)
+	}
+
+	db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
+	return db
+}
