82cd57e5b660fda06b6dbadd8629d8f9def26c41
Supakorn <supakorn0728@gmail.com>
2024-09-26 17:36:03 +0700

    :sparkles: logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,88 @@
 package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Supakornn/go-api/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+type ILogger interface {
+	Print() ILogger
+	Save()
+	SetQuery(c *fiber.Ctx)
+	SetBody(c *fiber.Ctx)
+	SetResponse(res any)
+}
+
+type Logger struct {
+	Time       string `json:"time"`
+	Ip         string `json:"ip"`
+	Method     string `json:"method"`
+	StatusCode int    `json:"status_code"`
+	Path       string `json:"path"`
+	Query      any    `json:"query"`
+	Body       any    `json:"body"`
+	Response   any    `json:"response"`
+}
+
+func InitLogger(c *fiber.Ctx, res any) ILogger {
+	log := &Logger{
+		Time:       time.Now().Local().Format("2006-01-02 15:04:05"),
+		Ip:         c.IP(),
+		Method:     c.Method(),
+		StatusCode: c.Response().StatusCode(),
+		Path:       c.Path(),
+	}
+	log.SetQuery(c)
+	log.SetBody(c)
+	log.SetResponse(res)
+	return log
+}
+func (l *Logger) Print() ILogger {
+	utils.Debug(l)
+	return l
+}
+
+func (l *Logger) Save() {
+	data := utils.Output(l)
+
+	filename := fmt.Sprintf("./assets/logs/logger_%v.txt", strings.ReplaceAll(time.Now().Format("2006-01-02 15:04:05"), "-", ""))
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	defer file.Close()
+	file.WriteString(string(data) + "\n")
+}
+
+func (l *Logger) SetQuery(c *fiber.Ctx) {
+	var body any
+	if err := c.QueryParser(&body); err != nil {
+		log.Printf("error parsing query: %v", err)
+	}
+	l.Query = body
+}
+
+func (l *Logger) SetBody(c *fiber.Ctx) {
+	var body any
+	if err := c.BodyParser(&body); err != nil {
+		log.Printf("error parsing body: %v", err)
+	}
+
+	switch l.Path {
+	case "v1/users/signup":
+		l.Body = "hidden"
+	default:
+		l.Body = body
+	}
+}
+
+func (l *Logger) SetResponse(res any) {
+	l.Response = res
+}
