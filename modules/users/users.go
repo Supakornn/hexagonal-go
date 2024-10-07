21cac2e9381db16357b991ef6235d68a3cc802ff
Supakorn <supakorn0728@gmail.com>
2024-10-08 01:16:32 +0700

    :sparkles: signin

--- modules/users/users.go
+++ modules/users/users.go
@@ -8,33 +8,45 @@ import (
 )
 
 type User struct {
-	ID       string `db: "id" json: "id"`
-	Email    string `db: "email" json: "email"`
-	Username string `db: "username" json: "username"`
-	RoleId   int    `db: "role_id" json: "role_id"`
+	ID       string `db:"id" json:"id"`
+	Email    string `db:"email" json:"email"`
+	Username string `db:"username" json:"username"`
+	RoleId   int    `db:"role_id" json:"role_id"`
 }
 
 type UserRegisterReq struct {
-	Email    string `db: "email" json: "email" form:"email"`
-	Password string `db: "password" json: "password" form:"password"`
-	Username string `db: "username" json: "username" form:"username"`
+	Email    string `db:"email" json:"email" form:"email"`
+	Password string `db:"password" json:"password" form:"password"`
+	Username string `db:"username" json:"username" form:"username"`
 }
-
 type UserPassport struct {
-	User  *User      `json: "user"`
-	Token *UserToken `json: "token"`
+	User  *User      `json:"user"`
+	Token *UserToken `json:"token"`
 }
 
 type UserToken struct {
-	Id           string `db: "id" json: "id"`
-	AccessToken  string `db: "access_token" json: "access_token`
-	RefreshToken string `db: "refresh_token" json: "refresh_token`
+	Id           string `db:"id" json:"id"`
+	AccessToken  string `db:"access_token" json:"access_token`
+	RefreshToken string `db:"refresh_token" json:"refresh_token`
+}
+
+type UserCredential struct {
+	Email    string `db:"email" json:"email" form:"email"`
+	Password string `db:"password" json:"password" form:"password"`
+}
+
+type UserCredentialCheck struct {
+	Id       string `db:"id"`
+	Email    string `db:"email"`
+	Password string `db:"password"`
+	Username string `db:"username"`
+	RoleId   int    `db:"role_id"`
 }
 
 func (obj *UserRegisterReq) BcryptHashing() error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(obj.Password), 10)
 	if err != nil {
-		return fmt.Errorf("Error hashing password: %v", err)
+		return fmt.Errorf("hashing password failed: %v", err)
 	}
 	obj.Password = string(hashedPassword)
 	return nil
--- modules/users/usersUsecases/usersUsecase.go
+++ modules/users/usersUsecases/usersUsecase.go
@@ -1,13 +1,17 @@
 package usersUsecases
 
 import (
+	"fmt"
+
 	"github.com/Supakornn/hexagonal-go/config"
 	"github.com/Supakornn/hexagonal-go/modules/users"
 	"github.com/Supakornn/hexagonal-go/modules/users/usersRepositories"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type IUserUsecase interface {
 	InsertCustomer(req *users.UserRegisterReq) (*users.UserPassport, error)
+	GetPassport(req *users.UserCredential) (*users.UserPassport, error)
 }
 
 type userUsecase struct {
@@ -33,4 +37,27 @@ func (u *userUsecase) InsertCustomer(req *users.UserRegisterReq) (*users.UserPas
 	}
 
 	return result, nil
+}
+
+func (u *userUsecase) GetPassport(req *users.UserCredential) (*users.UserPassport, error) {
+	user, err := u.userRepository.FindOneUserByEmail(req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return nil, fmt.Errorf("password is incorrect")
+	}
+
+	passport := &users.UserPassport{
+		User: &users.User{
+			ID:       user.Id,
+			Email:    user.Email,
+			Username: user.Username,
+			RoleId:   user.RoleId,
+		},
+		Token: nil,
+	}
+
+	return passport, nil
 }
